b6fa332c879971ea6e8a65870ccb9e3c0fc93151
huangyh <mrhuangyuhui@gmail.com>
2021-11-22 18:23:48 +0800

    上传代码

---
+++ src/main/java/org/example/ThreadPerMessage/A7_6b/Main.java
@@ -0,0 +1,13 @@
+package org.example.ThreadPerMessage.A7_6b;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) {
+        try {
+            new MiniServer(8888).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/ThreadPerMessage/README.md
@@ -0,0 +1,1 @@
+第 7 章 Thread-Per-Message 模式 —— 这项工作就交给你了
